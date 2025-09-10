d298efaa044343450265b7da2e0c12b988439c23
Heoyoon <parechae@gmail.com>
2025-09-10 19:50:49 +0900

    [fix] turnstateMachine CurrState누락부분 수정

--- Assets/04.Script/TurnBase/TurnDefine.cs
+++ Assets/04.Script/TurnBase/TurnDefine.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class TurnStateMachine : IStateMachineBase<TurnStateBase>
 {
+    TurnStateBase currState;
     public Dictionary<TurnTypes,TurnStateBase> states;
     public void ChangeState(TurnStateBase next)
     {
