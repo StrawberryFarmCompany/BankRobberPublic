36aeb3e1e0e8f21addaa7b07cd7ef434795061ca
Seungmin7979 <safetmdals79@gmail.com>
2025-09-14 21:27:55 +0900

    [ Feat ] 1인칭 플레이어 카메라 전환 및 실험 NPC 생성

--- Assets/04.Script/Manager/PlayerChangeManager.cs
+++ Assets/04.Script/Manager/PlayerChangeManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,11 +8,30 @@ public class PlayerChangeManager : MonoBehaviour
     public GameObject player2;
     public GameObject player3;
 
+    private Camera mainCam;
+
     private void Start()
     {
+        mainCam = Camera.main;
+
+        // 시작은 player1 시점
         OnStart();
+        SetCameraToPlayer(player1);
     }
 
+    // 카메라 시점 변환
+    private void SetCameraToPlayer(GameObject player)
+    {
+        Transform camHolder = player.transform.Find("CameraContainer");
+        if (camHolder != null && mainCam != null)
+        {
+            mainCam.transform.SetParent(camHolder);
+            mainCam.transform.localPosition = Vector3.zero;
+            mainCam.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    // 플레이어1로 변경
     public void OnPlayer1(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
@@ -21,9 +39,12 @@ public class PlayerChangeManager : MonoBehaviour
             player1.GetComponent<PlayerInput>().enabled = true;
             player2.GetComponent<PlayerInput>().enabled = false;
             player3.GetComponent<PlayerInput>().enabled = false;
+
+            SetCameraToPlayer(player1);
         }
     }
 
+    //플레이어2로 변경
     public void OnPlayer2(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
@@ -31,9 +52,12 @@ public class PlayerChangeManager : MonoBehaviour
             player1.GetComponent<PlayerInput>().enabled = false;
             player2.GetComponent<PlayerInput>().enabled = true;
             player3.GetComponent<PlayerInput>().enabled = false;
+
+            SetCameraToPlayer(player2);
         }
     }
 
+    //플레이어3로 변경
     public void OnPlayer3(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
@@ -41,10 +65,12 @@ public class PlayerChangeManager : MonoBehaviour
             player1.GetComponent<PlayerInput>().enabled = false;
             player2.GetComponent<PlayerInput>().enabled = false;
             player3.GetComponent<PlayerInput>().enabled = true;
+
+            SetCameraToPlayer(player3);
         }
     }
 
-    public void OnStart()
+    private void OnStart()
     {
         player2.GetComponent<PlayerInput>().enabled = false;
         player3.GetComponent<PlayerInput>().enabled = false;
