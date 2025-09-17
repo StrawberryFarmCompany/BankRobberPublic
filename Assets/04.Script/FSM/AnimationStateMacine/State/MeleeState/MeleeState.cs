8f4701deb824c5bae34d7d3ea6c59a6983b004b4
SJW1737 <sic470212@naver.com>
2025-09-17 21:59:34 +0900

    [Feat] 턴 관련 UI완성 및 스테이트에 따른 턴 종료 로직(수정 필요)

--- Assets/04.Script/FSM/AnimationStateMacine/State/MeleeState/MeleeState.cs
+++ Assets/04.Script/FSM/AnimationStateMacine/State/MeleeState/MeleeState.cs
@@ -11,6 +11,7 @@ public class MeleeState : AnimationState
 
     public override void Enter()
     {
+        base.Enter();
         Debug.Log("Enter Melee");
         animator.Play("Melee");
     }
@@ -18,6 +19,14 @@ public class MeleeState : AnimationState
     public override void Execute()
     {
         // Melee 동안 처리할 로직 (예: 입력 체크)
+        if (IsDone) return;
+
+        var info = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (info.normalizedTime >= 1f)
+        {
+            IsDone = true;
+        }
     }
 
     public override void Exit()
