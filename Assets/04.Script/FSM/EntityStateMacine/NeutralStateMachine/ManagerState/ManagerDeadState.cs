74377064bdfe081166aa3ead9f1d96259dfa860c
Seungmin7979 <safetmdals79@gmail.com>
2025-09-18 11:37:03 +0900

    [ Feat ] Entity 관련 FSM스크립트 추가 및 수정

--- Assets/04.Script/FSM/EntityStateMacine/NeutralStateMachine/ManagerState/ManagerDeadState.cs
+++ Assets/04.Script/FSM/EntityStateMacine/NeutralStateMachine/ManagerState/ManagerDeadState.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolEnemyInvestigateState : EntityState
+public class ManagerDeadState : NeutralState
 {
     public override void Enter()
     {
--- Assets/04.Script/FSM/EntityStateMacine/NeutralStateMachine/ManagerState/ManagerIdleCowerState.cs
+++ Assets/04.Script/FSM/EntityStateMacine/NeutralStateMachine/ManagerState/ManagerIdleCowerState.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolEnemyPatrolState : EntityState
+public class ManagerIdleCowerState : NeutralState
 {
     public override void Enter()
     {
