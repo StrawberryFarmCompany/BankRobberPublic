3be2c17c154679be92dd61760591fb06fbf36cb3
Seungmin7979 <safetmdals79@gmail.com>
2025-09-23 01:22:43 +0900

    [ Feat ] Manager, PatrolEnemy 스크립트 작성

--- Assets/04.Script/Entity/ManagerNPC.cs
+++ Assets/04.Script/Entity/ManagerNPC.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class ManagerNPC : NeutralNPC
 {
-    private bool canSeeAlly;
+    public bool canSeeAlly;
+    public int securityLevel = 1; 
 
     protected override void Awake()
     {
@@ -19,6 +20,7 @@ public class ManagerNPC : NeutralNPC
         nfsm.Current?.Execute();
     }
 
+    // 턴 마다 실행될 매서드
     public void OnTurnStart()
     {
         // 피격시 사망
@@ -27,14 +29,26 @@ public class ManagerNPC : NeutralNPC
             Die();
         }
 
-        // 플레이어 발견시 상태 변경
-        else if (canSeeAlly)
+        // 경계레벨이 2이하 또는 플레이어 발각시
+        else if (canSeeAlly == true && securityLevel <= 2)
         {
-            nfsm.ChangeState(nfsm.FindState(NeutralStates.ManagerIdleCowerState));
+            // Status에서 2턴의 목격자 Status를 받아옴. (경계수준 2미만일 경우 라고 되어 있는데 오타 인지 기획자 한테 물어보기)
+            // 턴 끝날 때 마다 목격자 status -= 1 해주기.
+
+            // if (status가 다시 0이 된다면)
+            // {
+            //     securityLevel += 1;
+            // }
+        }
+
+        // 경계레벨이 3일 때
+        else if (securityLevel == 3)
+        {
+            OnPlayerDetected();
         }
     }
 
-    // 피격시 사망
+    // 피격시 사망 // 위에서 Action에 추가 되어 있는데 추후에 해결하기.
     public void TakeDamage()
     {
         // if (주사위에서 대미지 받는 매서드 받아오기)
