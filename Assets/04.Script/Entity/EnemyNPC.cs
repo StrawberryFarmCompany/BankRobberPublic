4abbb6f316707b65a3fad7128fdda1c1ee2fd667
Heoyoon <parechae@gmail.com>
2025-09-27 00:43:28 +0900

    [feat]enemy turn 사전딜레이 추가

--- Assets/04.Script/Entity/EnemyNPC.cs
+++ Assets/04.Script/Entity/EnemyNPC.cs
@@ -34,17 +34,17 @@ public class EnemyNPC : MonoBehaviour
     protected virtual void CalculateBehaviour()
     {
 
-        if (GameManager.GetInstance.CurrentPhase == GamePhase.NoneBattle)
+        if (GameManager.GetInstance.CurrentPhase == GamePhase.NoneBattle)   
         {
             TaskManager.GetInstance.RemoveTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 1f));
-            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 1f));
-            TaskManager.GetInstance.StartTask();
+            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(() => { }, 1f));
+            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 0f));
         }
         else
         {
             TaskManager.GetInstance.RemoveTurnBehaviour(new TurnTask(GameManager.GetInstance.BattleTurn.ChangeState, 1f));
-            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.BattleTurn.ChangeState, 1f));
-            TaskManager.GetInstance.StartTask();
+            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(()=> { }, 1f));
+            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.BattleTurn.ChangeState, 0f));
         }
     }
     
