d86d8c77dfe45018e6ac03bc81f5222ed5bfb8f4
Heoyoon <parechae@gmail.com>
2025-09-26 13:31:48 +0900

    [feat]리소스 메니저 추가 버프 팩토리 추가,파싱객체 추가

--- Assets/04.Script/Interface/IBuff.cs
+++ Assets/04.Script/Interface/IBuff.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public interface IBuff
+namespace BuffDefine
 {
-    public BuffData Data { get;}
-    public EntityStats Target { get;}
-    
-    public void RegistBuff();
-    public void ReleaseBuff();
-    /*public static IBuff Factory(BuffData data,BuffType type)
-    {
 
-    }*/
+    public interface IBuff
+    {
+        public BuffData Data { get; }
+        public EntityStats Target { get; }
+
+        public void RegistBuff();
+        public void ReleaseBuff();
+        public static IBuff Factory(BuffData data, EntityStats stat, BuffType type)
+        {
+            switch (type)
+            {
+                case BuffType.moveBonus:
+                    return new MoveBonus(data, stat);
+                case BuffType.rallyCopCall:
+                    return new RallyCopCall(data, stat);
+                case BuffType.rallySecCall:
+                    return new RallySecCall(data, stat);
+                case BuffType.securityLevel:
+                    return new SecurityLevel(data, stat);
+                case BuffType.aiming:
+                    return new Aiming(data, stat);
+                case BuffType.witness:
+                    return new Witness(data, stat);
+                case BuffType.spotted:
+                    return new Spotted(data, stat);
+                case BuffType.healBan:
+                    return new HealBan(data, stat);
+                case BuffType.heal:
+                    return new Heal(data, stat);
+                default:
+                    Debug.LogError("등록되지 않은 버프 이넘값입니다 메세지를 눌러 확인해 주세요");
+                    return null;
+            }
+        }
+    }
+
 }
