c907433fef9f986dbd4f201da10c37e98ddfe1bf
Heoyoon <parechae@gmail.com>
2025-09-29 21:17:10 +0900

    [feat] 데이터테이블 연결

--- Assets/04.Script/Define/SecurityData.cs
+++ Assets/04.Script/Define/SecurityData.cs
@@ -6,20 +6,40 @@ public class SecurityData
 {
     private int secLevel;
     public int GetSecLevel { get { return secLevel; } }
-    private IBuff currSec;
-    public SecurityData(int defaultLevel = 1)
+    static IBuff battleSec;
+    IBuff currSec;
+
+    public IBuff GetSecBuff { get {return battleSec != null ? battleSec : currSec ; }}
+
+    /// <summary>
+    /// 시큐리티레벨 설정하는 함수
+    /// </summary>
+    /// <param name="defaultLevel">1~3 사이의 값을 넣어야합니다.</param>
+    public SecurityData(ushort defaultLevel = 1)
     {
         secLevel = defaultLevel;
+        SetSecLevel(defaultLevel);
     }
     /// <summary>
     /// 시큐리티레벨 설정하는 함수
     /// </summary>
-    /// <param name="level">0~2 사이의 값을 넣어야합니다.</param>
-    public void SetSecLevel(ushort level)
+    /// <param name="level">1~3 사이의 값을 넣어야합니다.</param>
+    private void SetSecLevel(ushort level)
     {
-        if (level >= 3 || level == secLevel) return;
-        ushort key = (ushort)(6000 + level);
+        if (level == 0 || level >= 3 || level == secLevel) return;
+        ushort key = (ushort)(6000 + level - 1);
+        secLevel = level;
         ResourceManager.GetInstance.GetBuffData.TryGetValue(key, out BuffData data);
-        currSec = IBuff.Factory(data, null, BuffType.securityLevel);
+
+        if (secLevel != 3)
+        {
+            currSec = IBuff.Factory(data, null, BuffType.securityLevel);
+        }
+        else
+        {
+            battleSec = IBuff.Factory(data, null, BuffType.securityLevel);
+        }
+
+            
     }
 }
