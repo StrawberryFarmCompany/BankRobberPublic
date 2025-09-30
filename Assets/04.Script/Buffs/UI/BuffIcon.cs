8c558f6a2aaf9f4af25e9efb8983d9d5aa582232
Heoyoon <parechae@gmail.com>
2025-10-01 05:07:42 +0900

    [feat] 버프 체크 UI 추가, 디버깅 진행

---
+++ Assets/04.Script/Buffs/UI/BuffIcon.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class BuffIcon : MonoBehaviour, IPointerClickHandler
+{
+    private Image image;
+    private int index;
+    public int Index { get { return index; } }
+    private BuffPannel Pannel { get { return UIManager.GetInstance.BuffPannel; } }
+    private void Awake()
+    {
+        Debug.Log("sex");
+    }
+    public void Init(int index)
+    {
+        if (image == null) image = GetComponent<Image>();
+        this.index = index;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("우클릭 감지됨");
+            Pannel.SetDescription(index);
+        }
+    }
+    public void SetImage(Sprite sprite)
+    {
+        image.sprite = null;
+        image.sprite = sprite;
+    }
+    public void Release()
+    {
+        image.sprite = null;
+    }
+}
