11800cec375b9743b922e5089bf3c47cae8c8653
Heoyoon <parechae@gmail.com>
2025-10-14 11:15:55 +0900

    [feat] vaultDoor 추가 및 작업 중

--- Assets/04.Script/Interactable/InteractionSetter.cs
+++ Assets/04.Script/Interactable/InteractionSetter.cs
@@ -9,14 +9,12 @@ public class InteractionSetter : MonoBehaviour
     [SerializeField] bool isWalkAble = false;
     private GameManager Manager {get{return GameManager.GetInstance; } }
 
-    [ConditionalHide("type", (int)InteractionType.Door, (int)InteractionType.KeyCard)]//금고문,문 카드키
+    [ConditionalHide("type", (int)InteractionType.Door, (int)InteractionType.KeyCard, (int)InteractionType.GoldBar)]//금고문,문 카드키
     public Transform target;
     [ConditionalHide("type", (int)InteractionType.Door, (int)InteractionType.KeyCard)]//금고문,문 카드키
     public int doorValue;
     [ConditionalHide("type", (int)InteractionType.Door)]//금고문,문
     public DoorLockType lockType;
-    [ConditionalHide("type", (int)InteractionType.GoldBar)]//골드바, 현금
-    public GameObject[] consumeItems;
     void Start()
     {
         interaction = IInteractable.Factory(type);
@@ -47,13 +45,33 @@ public class InteractionSetter : MonoBehaviour
                 break;
             case InteractionType.GoldBar:
                 GoldBar gold = (GoldBar)interaction;
-                gold.Init(pos, consumeItems);
+                gold.Init(pos, target.gameObject);
                 break;
             case InteractionType.MoneyBag:
                 break;
             case InteractionType.KeyCard:
                 KeyCard keyCard = (KeyCard)interaction;
                 keyCard.Init(pos,target, doorValue);
+                break;
+            case InteractionType.VaultDoor:
+                VaultDoor vaultDoor = (VaultDoor)interaction;
+                Vector3Int[] doorPoints = new Vector3Int[2];
+                for (int i = 1; i <= 4; i++)
+                {
+                    Vector3Int currPos = pos + (GameManager.GetInstance.GetVecInt(-transform.right * i));
+                    GameManager.GetInstance.Nodes[currPos].isWalkable = i == 2 || i == 3 ? true : false;
+                    if (i == 2 || i == 3)
+                    {
+                        GameManager.GetInstance.Nodes[currPos].isWalkable = true;
+                        doorPoints[i - 2] = currPos;
+                    }
+                    else
+                    {
+                        GameManager.GetInstance.Nodes[currPos].isWalkable = false;
+                    }
+
+                }
+                vaultDoor.Init(doorPoints,target,doorValue);
                 break;
             default:
                 break;
