ea8c412494bf5770bf52990104a33932631f1e74
Heoyoon <parechae@gmail.com>
2025-10-16 14:05:32 +0900

    [fix] 버튼 인터렉터블 설정 및 잘못된 이벤트등록 변경

--- Assets/04.Script/Interactable/InteractionSetter.cs
+++ Assets/04.Script/Interactable/InteractionSetter.cs
@@ -69,9 +69,12 @@ public class InteractionSetter : MonoBehaviour
                     {
                         GameManager.GetInstance.Nodes[currPos].isWalkable = false;
                     }
-
                 }
                 vaultDoor.Init(doorPoints,target,doorValue);
+                break;
+            case InteractionType.EscapeCar:
+                EscapeCar car = (EscapeCar)interaction;
+                car.Init(pos);
                 break;
             default:
                 break;
