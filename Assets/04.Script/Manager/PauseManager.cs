c1cb12db0433b72d6f8c56ceb25dbc6d15607e70
SJW1737 <sic470212@naver.com>
2025-10-17 17:20:37 +0900

    [Feat] 1인칭 감도 및 씬 이동 추가

--- Assets/04.Script/Manager/PauseManager.cs
+++ Assets/04.Script/Manager/PauseManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
@@ -17,7 +18,10 @@ public class PauseManager : MonoBehaviour
     [Header("옵션 UI")]
     [SerializeField] private Button backButton;
 
-    private bool isPaused = false;
+    public static bool isPaused { get; private set; } = false;
+
+    private const string LOBBY_SCENE = "LobbyPlayerChange";
+    private const string TITLE_SCENE = "MainTitleUIScene";
 
     void Start()
     {
@@ -27,14 +31,14 @@ public class PauseManager : MonoBehaviour
         if (resumeButton != null) resumeButton.onClick.AddListener(ResumeGame);
         if (optionButton != null) optionButton.onClick.AddListener(OpenOption);
         if (quitButton != null) quitButton.onClick.AddListener(QuitGame);
-        if (backButton) backButton.onClick.AddListener(CloseOption);
+        if (backButton != null) backButton.onClick.AddListener(CloseOption);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (optionUI.activeSelf)
+            if (optionUI && optionUI.activeSelf)
             {
                 CloseOption();
                 return;
@@ -50,6 +54,10 @@ public class PauseManager : MonoBehaviour
         Time.timeScale = 0f;
         pausePanel.SetActive(true);
         optionUI.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
         isPaused = true;
     }
 
@@ -58,25 +66,47 @@ public class PauseManager : MonoBehaviour
         Time.timeScale = 1f;
         pausePanel.SetActive(false);
         optionUI.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         isPaused = false;
     }
 
     private void OpenOption()
     {
-        Debug.Log("옵션 메뉴 열기");
-        pausePanel.SetActive(false);
-        optionUI.SetActive(true);
+        if (pausePanel) pausePanel.SetActive(false);
+        if (optionUI) optionUI.SetActive(true);
     }
 
     private void CloseOption()
     {
-        Debug.Log("옵션 메뉴 닫기");
-        pausePanel.SetActive(true);
-        optionUI.SetActive(false);
+        if (pausePanel) pausePanel.SetActive(true);
+        if (optionUI) optionUI.SetActive(false);
     }
 
     private void QuitGame()
     {
-        Debug.Log("메인메뉴 혹은 로비 이동");
+        Time.timeScale = 1f;
+        isPaused = false;
+        if (pausePanel) pausePanel.SetActive(false);
+        if (optionUI) optionUI.SetActive(false);
+
+        string cur = SceneManager.GetActiveScene().name;
+
+        //로비에서 메인타이틀
+        if (cur == LOBBY_SCENE)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            SceneManager.LoadScene(TITLE_SCENE);
+        }
+        //인게임에서 로비
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            SceneManager.LoadScene(LOBBY_SCENE);
+        }
     }
 }
