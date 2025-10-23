91deb67d2c862067cc22d15343559235ab954bb3
adorablenewbie <youngjong2965@gmail.com>
2025-10-23 10:38:15 +0900

    [ Feat ] 도장 애니메이션

--- Assets/04.Script/Manager/NodePlayerManager.cs
+++ Assets/04.Script/Manager/NodePlayerManager.cs
@@ -178,7 +178,6 @@ public class NodePlayerManager : MonoBehaviour
             if (players.Count == 0)
             {
                 currentPlayerIndex = 0;
-                GameManager.GetInstance.GameEnd();
                 return;
             }
 
@@ -245,18 +244,18 @@ public class NodePlayerManager : MonoBehaviour
         }
     }
 
-    public void SetEscapeCondition(EntityStats stats)
+    public void SetEscapeCondition(EntityStats stats, EscapeCondition condition)
     {
         switch (stats.characterType) 
         {
             case (CharacterType.Bishop):
-                isEscapeBishop = EscapeCondition.Escape;
+                isEscapeBishop = condition;
                 break;
             case (CharacterType.Rook):
-                isEscapeRook= EscapeCondition.Escape;
+                isEscapeRook= condition;
                 break;
             case (CharacterType.Knight):
-                isEscapeKnight = EscapeCondition.Escape;
+                isEscapeKnight = condition;
                 break;
             default:
                 break;
@@ -308,7 +307,9 @@ public class NodePlayerManager : MonoBehaviour
 
     private IEnumerator LateGameEndCallCoroutine()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1.5f);
         GameManager.GetInstance.GameEnd();
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = 0f;
     }
 }
--- Assets/04.Script/Skills/EntityStats.cs
+++ Assets/04.Script/Skills/EntityStats.cs
@@ -172,6 +172,7 @@ public class EntityStats
 
     private void Dead()
     {
+        NodePlayerManager.GetInstance.SetEscapeCondition(this, EscapeCondition.Arrest);
         OnDead?.Invoke();
         DestroyEntity();
         //GameManager.GetInstance.사망으로 인해 발생할 게임내 상황을 정의
@@ -181,14 +182,6 @@ public class EntityStats
         }
         if (NodePlayerManager.GetInstance.GetAllPlayers().Count <= 0)
         {
-            if(NodePlayerManager.GetInstance.GetEscapeSuccess() == GameResult.Failed)
-            {
-                UIManager.GetInstance.gameEndUI.SetFail();
-            }
-            else
-            {
-                UIManager.GetInstance.gameEndUI.SetSuccess();
-            }
             NodePlayerManager.GetInstance.LateGameEndCall();
         }
     }
