f3f7651c7d03be00d12dc1eba2540697c2254aa8
SJW1737 <sic470212@naver.com>
2025-10-24 18:04:17 +0900

    [Feat] 특성 상점 마무리 및 구매 관련 돈 추가 스크립트 및 테스트 확인

---
+++ Assets/04.Script/Lobby/Shop/TestAddMoney.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TestAddMoney : MonoBehaviour
+{
+    [SerializeField] private KeyCode addKey = KeyCode.M;
+    [SerializeField] private int addAmount = 100000;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(addKey))
+        {
+            Money.Add(addAmount);
+            Debug.Log($"[테스트] {addAmount:N0}원 추가됨 → 현재 잔액: {Money.Value:N0}");
+        }
+    }
+}
