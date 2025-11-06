54ef650415b7d3aec757a9302c45635881c8f505
adorablenewbie <youngjong2965@gmail.com>
2025-11-06 22:11:15 +0900

    [ Feat ] 튜토리얼 제작 중

---
+++ Assets/04.Script/Interface/ITrap.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITrap
+{
+    public void Init();
+    public void OnStanding(EntityStats stat);
+
+}
