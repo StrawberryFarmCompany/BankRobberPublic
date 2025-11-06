21878103df85b5d3c265bab7e4b9631aec3aab58
SJW1737 <sic470212@naver.com>
2025-11-06 17:33:17 +0900

    [Fix] 업그레이드 관련 수정 및 가이드 널 수정

--- Assets/04.Script/Skills/EntityStats.cs
+++ Assets/04.Script/Skills/EntityStats.cs
@@ -219,6 +219,10 @@ public class EntityStats
         if (CurHp > maxHp)
         {
             CurHp = maxHp;
+        }
+
+        if (hpbar != null)
+        {
             hpbar.SetCurrHP(CurHp);
         }
     }
