b990c65ba6a3e27a2a273e907751df70dcf36df4
SJW1737 <sic470212@naver.com>
2025-11-14 18:38:15 +0900

    [Feat] 재장전UI, 행동 버튼 툴팁UI 추가

---
+++ Assets/04.Script/UI/CommandPanel/ActionTooltipTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActionTooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [TextArea]
+    public string description;   // 버튼마다 직접 입력
+
+    private RectTransform rect;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UIManager.GetInstance.actionTooltip.Show(description, rect);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UIManager.GetInstance.actionTooltip.Hide();
+    }
+}
