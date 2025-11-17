a9e32f43d3b78ee71b55fded63cab66e607a2edd
Seungmin7979 <safetmdals79@gmail.com>
2025-11-17 15:11:21 +0900

    [ Chore ] 불필요한 코드 수정

--- Assets/04.Script/Entity/EnemyNPC.cs
+++ Assets/04.Script/Entity/EnemyNPC.cs
@@ -50,11 +50,9 @@ public class EnemyNPC : MonoBehaviour
     {
         stats.ResetForNewTurn(); // 행동력 및 이동력 초기화
 
-        if (GameManager.GetInstance.CurrentPhase == GamePhase.NoneBattle)   
-        {
-            TaskManager.GetInstance.RemoveTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 1f));
-            TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 0f));
-        }
+        TaskManager.GetInstance.RemoveTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 1f));
+        TaskManager.GetInstance.AddTurnBehaviour(new TurnTask(GameManager.GetInstance.NoneBattleTurn.ChangeState, 0f));
+
         stats.NodeUpdates(transform.position);
     }
 
